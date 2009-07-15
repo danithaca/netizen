d23120369b575d9212b3eb808f3b5086fcdb5496
danithaca <danithaca@eb1c0644-df3f-11dd-a15c-5b57b35736ad>
2009-07-15 20:55:57 +0000

    add url generate script

---
+++ trunk/src/magicstudio/netizen/util/GenerateURLs.groovy
@@ -0,0 +1,8 @@
+def tianyaNews = ["http://www.tianya.cn/publicforum/content/news/1/%i.shtml", 75000, 135000]
+new File("output.txt").withWriter { writer ->
+  (tianyaNews[1]..tianyaNews[2]).each {
+    //println tianyaNews[0].replace('%i', it.toString())
+    //writer << tianyaNews[0].replace('%i', it) << '\n';
+    writer.writeLine tianyaNews[0]
+  }
+}
