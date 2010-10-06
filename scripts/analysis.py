0c4e19d499ca77c14ea373fec7fae59a501b9cbe
danithaca <danithaca@eb1c0644-df3f-11dd-a15c-5b57b35736ad>
2010-10-06 17:58:12 +0000

    check in from si desktop

--- scripts/analysis.py
+++ scripts/analysis.py
@@ -20,9 +20,94 @@ def output_centrality_csv(netfile, output):
     print >>o, ','.join(map(str, [i, v['id'], degree[i], betweenness[i], closeness[i]]))
   o.close()
 
+def output_term_knn(netfile, term, limit=0):
+  g = graph_info(netfile)
+  # find the node id or "vertex" object for the term
+  v = g.vs.select(id_eq = term)
+  if len(v) != 1:
+    print "can't find term", term
+  v = v[0]
+  # find the adjacent verteces (neighbor)
+  neighbors = g.neighbors(v.index)
+  print "a total of", len(neighbors), "neighbors"
+  # find the adjacent edges with weights
+  adjedges = g.adjacent(v.index)
+  assert len(neighbors) == len(adjedges)
+  l = []
+  knnlist = []
+  for e in adjedges:
+    if g.is_loop(e):
+      print "contains self loop", e
+      continue
+    e = g.es[e]
+    o = e.target if e.target!=v.index else e.source
+    l.append((o, e['weight']))
+  l.sort(cmp=lambda x,y: cmp(x[1],y[1]), reverse=True)
+  for t in l:
+    limit -= 1
+    if limit == 0: break
+    knnlist.append((g.vs[t[0]]['id'], t[1]))
+    print "%s\t%d" % (g.vs[t[0]]['id'], t[1])
+  return knnlist
 
+
+# TODO: think about how to do this. comparing the rank difference between knn from 2 networks
+def term_knn_diff(f1, f2, term, knn_cutoff=100):
+  # knn1 is the list of knn from the first network
+  knn1 = output_term_knn(f1, term)
+  knn1 = [t[0] for t in knn1] # only take the term, not the weight
+  knn1_len = len(knn1)
+  knn2 = output_term_knn(f2, term)
+  knn2 = [t[0] for t in knn2]
+  knn2_len = len(knn2)
+  
+  diff = []
+  visited = []
+  for i in range(min(knn1_len, knn_cutoff)):
+    term = knn1[i]
+    if term in knn2:
+      j = knn2.index(term)
+      visited.append(term)
+    else:
+      j = knn2_len
+    diff.append((term, i, j, abs(i-j)))
+  for j in range(min(knn2_len, knn_cutoff)):
+    term = knn2[j]
+    if term in visited: continue
+    if term in knn1:
+      i = knn1.index(term)
+    else:
+      i = knn1_len
+    diff.append((term, i, j, abs(i-j)))
+  
+  # sorting
+  diff.sort(cmp=lambda x,y: cmp(x[3], y[3]), reverse=True)
+  print "printing difference!"
+  for d in diff:
+    print '\t'.join(map(str, d))
+    
+
+
+def graph_info(netfile):
+  g = igraph.read(netfile)
+  #print "edges attributes:", g.edge_attributes()
+  #print "edges attributes:", g.vertex_attributes()
+  if g.is_directed(): print "directed graph"
+  else: print "indirected graph"
+  
+#  for v in g.vs:
+#    print v
+#  for e in g.es:
+#    print e
+#  v = g.vs.select(id_eq = ur'夏')
+#  print v[0]['shape']
+  return g
 
 if __name__ == '__main__':
-  os.chdir('..')
+  os.chdir('../data4tech')
   print os.getcwd()
-  output_centrality_csv('data/tigerpeople.net', 'data/tiger-people.csv')
+  #output_centrality_csv('data/tigerpeople.net', 'data/tiger-people.csv')
+  #graph_info('test.net')
+  #output_term_knn('peoplefull.net', ur'法律')
+  term_knn_diff('tianyafull.net', 'peoplefull.net', ur'法律')
+
