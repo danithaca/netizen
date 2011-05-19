a3a5695cddf44922f76a66df9d44a07088a6ed2d
danithaca <danithaca@eb1c0644-df3f-11dd-a15c-5b57b35736ad>
2011-05-19 11:17:31 +0000

    added randomwalk

---
+++ trunk/scripts/randomwalk.py
@@ -0,0 +1,20 @@
+# this is a simple example of random walk.
+
+class BaseRandomWalk(object):
+  pass
+  
+
+
+class PageRank(BaseRandomWalk):
+  pass
+  
+
+
+class AbsorbingRandomWalk(BaseRandomWalk):
+  pass
+  
+
+
+class LocalConsistencyGlobalConsistency(BaseRandomWalk):
+  pass
+  
